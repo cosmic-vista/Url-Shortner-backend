952582e60d3acf0794851f5bb5f7bbfa0b95ab9b
cosmic-vista <akkashyadav61@gmail.com>
2025-09-21 21:18:41 +0530

    changes

--- controller/urlController.js
+++ controller/urlController.js
@@ -11,6 +11,7 @@ export const createShortUrl = async (req, res) => {
   }
 
   const user = await User.findById(userId);
+
   if (!user) {
     return res.status(404).json({ error: "User not found" });
   }
